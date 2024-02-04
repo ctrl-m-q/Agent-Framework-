9685d03f53cdc73d4af5ed523a11a597fec0b9d9
Quadri Jemimah <64306905+ctrl-m-q@users.noreply.github.com>
2024-02-04 06:01:03 +0200

    Update main.py
    properly formatting the "topic " format to allow user input for whatever topic

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import app
-if __name__== "__main__":
-    topic  = "#ask user what topic"
+
+if __name__ == "__main__":
+    topic = input("Enter the topic: ")
+
+    # Pass the topic to the methods in app.py
+    app.researcher_task = "give ideas for teaching a newbie {}.".format(topic)
+    app.writer_role = "Use the Researcher’s ideas to write a piece of text to explain the {}.".format(topic)
+    app.examiner_role = "Craft 3 test questions to evaluate the newbie's understanding of the created text, along with the correct answers.".format(topic)
+
+
+    openhermes_model = app.openhermes_model
 
 
 
