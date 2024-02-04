c8ddee921ee9fa51144906450e77c57bacfaf4b9
Quadri Jemimah <64306905+ctrl-m-q@users.noreply.github.com>
2024-02-04 05:58:45 +0200

    Update app.py
    had git crash process locally, copied and pasted changes manually from the local machine. Deleting files and re-cloning the repository. Changes in the commit include proper import of ollama, and tweaking prompting.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import autogen #importing autogen
+from ollama import openhermes #import ollama library
 
 #initialise openhermes model
-openhermes_model = autogen.load_model("openhermes")
+openhermes_model = openhermes
+
 
 config_list = [
     {
@@ -19,19 +21,23 @@ llm_config={
 
 researcher = autogen.AssistantAgent(
     name="researcher",
-    llm_config=llm_config)
+    llm_config=llm_config
+    )
 
 
 writer = autogen.AssistantAgent(
-   name= "writer", llm_config=llm_config)
+   name= "writer", llm_config=llm_config
+   )
 
 examiner = autogen.AssistantAgent(
-    name="examiner", llm_config=llm_config)
+    name="examiner", llm_config=llm_config
+    )
 
 #assign roles to agents
-researcher_task = "Develop ideas for teaching someone new to the f'{{topic}}"
-writer_role = "Use the Researcher’s ideas to write a piece of text to explain the f'{{topic}}"
-examiner_role = "Craft 2-3 test questions to evaluate understanding of the created text, along with the correct answers. In other words: test whether a student has fully understood the text."
+
+researcher_task = "give ideas for teaching a newbie {}.".format(topic)
+writer_role = "Use the Researcher’s ideas to write a piece of text to explain the {}.".format(topic)
+examiner_role = "Craft 3 test questions to evaluate the newbie's understanding of the created text, along with the correct answers.".format(topic)
 
 user_proxy = autogen.UserProxyAgent(
     "user_proxy",
@@ -42,25 +48,15 @@ user_proxy = autogen.UserProxyAgent(
 
 #agents interfacing
 
-
-
 user_proxy.initiate_chat(researcher, message=researcher_task,  clear_history=False)
 response1 = researcher.get_response()
 generated_response_r = researcher.a_send(response1, recipient=writer) #generate response from user query
 writer.a_receive(generated_response_r, sender=researcher) #get the generated response from researcher and act on it
-user_proxy.initiate_chat(writer, message=writer_role, clear_history=False)#prompt the writer agent with task
+#user_proxy.initiate_chat(writer, message=writer_role, clear_history=False)#prompt the writer agent with task
 response2 = writer.get_response()
 generated_response_w = writer.a_send(response2, recipient=examiner) #generate response from user query
 examiner.a_receive(generated_response_w, sender=writer)
 user_proxy.initiate_chat(examiner, message=examiner_role, clear_history=False)
 
 
-
-
-
-#how do i generate response from the query given and parse this response into the other agents?
-#should i just use these simple prompts or use what's above?
-
-task2 = "Use the Researcher’s ideas to write a piece of text to explain the topic"
-user_proxy.initiate_chat(writer, message=task2, clear_history=False)
 
